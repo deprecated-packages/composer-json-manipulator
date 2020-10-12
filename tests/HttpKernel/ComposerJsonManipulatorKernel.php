f2f30a1f74390d10140f096a96a9362d262eef46
Tomas Votruba <tomas.vot@gmail.com>
2020-10-13 01:13:28 +0200

    [EasyHydrator] Make use rector split package (#2360)

---
+++ tests/HttpKernel/ComposerJsonManipulatorKernel.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\ComposerJsonManipulator\Tests\HttpKernel;
+
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symplify\ComposerJsonManipulator\ComposerJsonManipulatorBundle;
+use Symplify\SymplifyKernel\Bundle\SymplifyKernelBundle;
+use Symplify\SymplifyKernel\HttpKernel\AbstractSymplifyKernel;
+
+final class ComposerJsonManipulatorKernel extends AbstractSymplifyKernel
+{
+    /**
+     * @return BundleInterface[]
+     */
+    public function registerBundles(): array
+    {
+        return [new ComposerJsonManipulatorBundle(), new SymplifyKernelBundle()];
+    }
+}
